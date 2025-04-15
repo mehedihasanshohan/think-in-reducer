5f089987d9ecd6cc2c3d2145f14e87388b79e562
mehedi hasan <mhshohan04@gmail.com>
2025-04-15 09:23:55 +0600

    project setup complete with tailwind v4.1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h2 className='text-rose-200 font-bold text-2xl text-center'>React Reducer</h2>
+
+   </>
+  )
+}
+
+export default App
